f8b0c140df5fc7c21e20ba2dc92591f796077f02
Philip Krause <philip.krause88@yahoo.de>
2022-02-19 18:35:41 +0100

    article model added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
+  enum category: { cash: 0, securities: 1, real_estate: 2, raw_materials: 3, crypto: 4, cars: 5, others: 6 }
+
   validates :name, :meta_title, :meta_description, :slug, :content, :category, :author, presence: true
 end
