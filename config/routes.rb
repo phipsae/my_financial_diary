7390834e0da80ae21b1f3a9ed8625d9419a3baf2
Philip Krause <philip.krause88@yahoo.de>
2022-02-22 20:04:17 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'landing_page#home'
   get '/dashboard', to: 'dashboard#index'
-  resources :asset
+  resources :asset, only: [ :index, :show, :create, :update, :destroy ]
 end
