1cff05b03e9ccf7483012363085b6456bf8b8faf
Philip Krause <philip.krause88@yahoo.de>
2022-02-26 12:35:25 +0100

    schema changed

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,8 +1,23 @@
 class Asset < ApplicationRecord
   belongs_to :user
-  belongs_to :asset_kind
   has_many :price_points, dependent: :destroy
   has_many :real_estates, dependent: :destroy
 
-  validates :name, presence: true
+  enum category: {
+    cash: 0,
+    securities: 1,
+    real_estate: 2,
+    raw_materials: 3,
+    crypto: 4,
+    cars: 5,
+    others: 6
+  }
+
+  enum securities: {
+    bonds: 0,
+    shares: 1,
+    fonds: 2
+  }
+
+  validates :name, :category, :sub_category, presence: true
 end
--- db/migrate/20220220165146_change_mortage_to_mortgage_in_real_estate.rb
+++
@@ -1,5 +0,0 @@
-class ChangeMortageToMortgageInRealEstate < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :real_estates, :mortage, :mortgage
-  end
-end
