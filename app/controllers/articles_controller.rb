2aaeea33d184db4c8003fde41237a7651e6e74ad
Ilan Enrique Castanares Gutman <ie.castanares@gmail.com>
2022-02-26 12:30:01 +0100

    blog working back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,13 @@
 class ArticlesController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index]
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, except: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: [:index, :show], unless: :skip_pundit?
+  after_action :verify_policy_scoped, except: [:index, :show], unless: :skip_pundit?
   def index
     @articles = Article.all
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   # Uncomment when you *really understand* Pundit!
