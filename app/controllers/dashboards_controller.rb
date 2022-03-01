2677f15387e039df57971b147204fbdb6001c11d
Philip Krause <philip.krause88@yahoo.de>
2022-03-01 21:35:51 +0100

    master merged from kevin

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,14 +2,9 @@ class DashboardsController < ApplicationController
   def index
     @assets = policy_scope(Asset.all)
     # asset_kind
-<<<<<<< HEAD:app/controllers/dashboard_controller.rb
-    asset_controller = AssetController.new
-    @all_assets_hash = asset_controller.index_all_assets(current_user)
-=======
-    asset_controller = AssetsController.new
-    @all_assets_hash = asset_controller.index_all_assets
->>>>>>> master:app/controllers/dashboards_controller.rb
-    @total_value = asset_controller.calculate_total_value(@all_assets_hash)
+    assets_controller = AssetsController.new
+    @all_assets_hash = assets_controller.index_all_assets(current_user)
+    @total_value = assets_controller.calculate_total_value(@all_assets_hash)
     # price_points
     price_point_controller = PricePointsController.new
     @price_points = price_point_controller.index_pp(params[:query])
