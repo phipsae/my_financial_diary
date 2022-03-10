6be8649068deeddc42d6f4dc5ceb3a8a879f42b6
Kevin Urbaum <kevin.urbaum@gmail.com>
2022-03-10 20:23:04 +0100

    first ajax api call

--- app/controllers/price_points_controller.rb
+++ app/controllers/price_points_controller.rb
@@ -3,9 +3,9 @@ class PricePointsController < ApplicationController
 
   def index_pp(params, user)
     if params.present?
-      @price_points = PricePoint.where(asset: Asset.where(category: params, user_id: user)).order(date: :desc, id: :desc) # user_id: current_user, DONT forget to add
+      @price_points = PricePoint.where(asset: Asset.where(category: params, user_id: user)).order(date: :desc, id: :desc).limit(4) # user_id: current_user, DONT forget to add
     else
-      @price_points = PricePoint.where(asset: Asset.where(user_id: user)).order(date: :desc, id: :desc) # user_id: current_user, DONT forget to add
+      @price_points = PricePoint.where(asset: Asset.where(user_id: user)).order(date: :desc, id: :desc).limit(4) # user_id: current_user, DONT forget to add
     end
   end
 
--- app/javascript/controllers/coin_list_controller.js
+++ app/javascript/controllers/coin_list_controller.js
@@ -4,16 +4,11 @@ export default class extends Controller {
   static targets = [ "form", "list", "amount", "symbol","number", "submit" ]
 
   update() {
-    event.preventDefault()
-    const apiKey = process.env.COINMARKETCAP_API_KEY
-    const url = `https://pro-api.coinmarketcap.com/v2/tools/price-conversion?amount=${this.amountTarget.value}&symbol=${this.symbolTarget.value}`
-    fetch(url, { headers: {'X-CMC_PRO_API_KEY': apiKey, 'Accept': 'application/json' } })
-      .then(response => response.json())
-      .then((data) => {
-        const cryptoValue = data.data[0]["quote"].USD.price
-        this.listTarget.value = cryptoValue;
-        this.numberTarget.value = this.amountTarget.value;
-        console.log(cryptoValue)
-      })
+  const url = `${this.formTarget.action}?amount=${this.amountTarget.value}&symbol=${this.symbolTarget.value}`
+  fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      this.listTarget.outerHTML = data;
+    })
   }
 }
