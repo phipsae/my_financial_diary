9ab2fcec3ea26d71c702010c6772b319502284cd
Kevin Urbaum <kevin.urbaum@gmail.com>
2022-03-12 16:19:51 +0100

    additional changes

---
+++ app/javascript/controllers/price_point_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "form", "pricepoint", "query", "more", "submit" ]
+
+  assetsUpdate() {
+  const url = `${this.formTarget.action}?query=${this.queryTarget.value}&more=${this.moreTarget.value}`
+  fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      this.pricepointTarget.outerHTML = data;
+    })
+  }
+
+  dashboardUpdate() {
+  const url = `${this.formTarget.action}?query=${this.queryTarget.value}&more=${this.moreTarget.value}`
+  fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      this.pricepointTarget.outerHTML = data;
+    })
+  }
+}
