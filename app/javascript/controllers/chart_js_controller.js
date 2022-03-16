431d94e6f6f06cb920925ad1021bb4757e60454e
Philip Krause <philip.krause88@yahoo.de>
2022-03-16 22:43:02 +0100

    cash fixed, charts designed

--- app/javascript/controllers/chart_js_controller.js
+++ app/javascript/controllers/chart_js_controller.js
@@ -1,17 +1,25 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["button", "line", "pie"]
-  static classes = ["hidden"]
+  static targets = ["buttonPie", "buttonLine", "line", "pie"]
+  static classes = ["hidden", "chart-btn", "chart-btn-active"]
 
   hiddenPie() {
     this.pieTarget.classList.add(this.hiddenClass)
     this.lineTarget.classList.remove(this.hiddenClass)
+    this.buttonLineTarget.classList.add("chart-btn-active")
+    this.buttonLineTarget.classList.remove("chart-btn")
+    this.buttonPieTarget.classList.remove("chart-btn-active")
+    this.buttonPieTarget.classList.add("chart-btn")
     // this.pieTarget.setAttribute("hidden", "")
   }
 
   hiddenLine(){
     this.lineTarget.classList.add(this.hiddenClass)
     this.pieTarget.classList.remove(this.hiddenClass)
+    this.buttonPieTarget.classList.add("chart-btn-active")
+    this.buttonPieTarget.classList.remove("chart-btn")
+    this.buttonLineTarget.classList.remove("chart-btn-active")
+    this.buttonLineTarget.classList.add("chart-btn")
   }
 }
